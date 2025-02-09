e9c3b12ecfb651f90bc51a0aa1a7b6b3fc7ae484
Mario Possamato <51346942+mariopossamato@users.noreply.github.com>
2025-02-09 04:47:52 -0500

    Update TextSetter.hpp

--- include/Lp/UI/TextSetter.hpp
+++ include/Lp/UI/TextSetter.hpp
@@ -1,3 +1,12 @@
 #ifndef LP_UI_TEXTSETTER_HPP
 #define LP_UI_TEXTSETTER_HPP
+
+#include <sead/ListNode.hpp>
+
+namespace Lp::UI {
+class TextSetter {
+  sead::ListNode m_ListNode;
+};
+} // namespace Lp::UI
+
 #endif
